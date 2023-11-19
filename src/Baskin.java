0c489e71144b603b77d95d9c1cdf41d9f987f14c
isaacj <duckhunterjr@gmail.com>
2023-11-19 14:14:23 -0800

    add pt 1 of recursive function

---
+++ src/Baskin.java
@@ -0,0 +1,15 @@
+public class Baskin {
+    public static int baskin(int num) {
+        // If 1 or 0, possibility counts as 1 for adding possibilities recursively
+        if (num == 1) {
+            return 1;
+        } else if (num == 0) {
+            return 1;
+        }
+        // Recursively add possibilities, each time removing 1 or 2 from num
+        return baskin(num - 1) + baskin(num - 2);
+    }
+    public static void main(String[] args) {
+        System.out.println(baskin(5));
+    }
+}
