a3468b31e781283c2b2dc90412ddea28d859c6da
isaacj <duckhunterjr@gmail.com>
2023-11-19 14:15:51 -0800

    refactor logic

--- src/Baskin.java
+++ src/Baskin.java
@@ -1,11 +1,8 @@
 public class Baskin {
     public static int baskin(int num) {
         // If 1 or 0, possibility counts as 1 for adding possibilities recursively
-        if (num == 1) {
+        if (num == 1 || num == 0) {
             return 1;
-        } else if (num == 0) {
-            return 1;
-        }
         // Recursively add possibilities, each time removing 1 or 2 from num
         return baskin(num - 1) + baskin(num - 2);
     }
