e6bd508666b252792de5aa2aa582bdedfa94cb49
isaacj <duckhunterjr@gmail.com>
2023-11-19 14:17:13 -0800

    fix syntax

--- src/Baskin.java
+++ src/Baskin.java
@@ -3,6 +3,7 @@ public class Baskin {
         // If 1 or 0, possibility counts as 1 for adding possibilities recursively
         if (num == 1 || num == 0) {
             return 1;
+        }
         // Recursively add possibilities, each time removing 1 or 2 from num
         return baskin(num - 1) + baskin(num - 2);
     }
